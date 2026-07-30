9207892e70f1c81db1d1e64c4a85dfad48cea870
Ben Samy <bensamy10@gmail.com>
2026-07-30 14:59:30 -0700

    Added eventListener for mouse hover over boxes

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ const container = document.getElementById('container');
 function makeDivs(numDivs) {
   for (let d = 0; d < numDivs; d++) {
     let cells = document.createElement('div');
+    cells.classList.add('boxes')
     container.appendChild(cells)
   }
 }
 
 makeDivs(256);
+
+const boxes = document.querySelectorAll('.boxes')
+
+boxes.forEach(box => {
+  box.addEventListener('mouseenter', () => {
+  // console.log("clicked");
+  box.style.backgroundColor = "#a9a9a9";
+  });
+});
